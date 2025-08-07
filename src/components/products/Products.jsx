e86b4a8a00dfd8a1fdd5eb3ae18d542a9b3b6920
nerina <ncoronel@primerosystems.com>
2025-08-07 19:48:02 -0300

    Added showDollar toggle

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { allProducts } from "../data/products";
 import ProductItem from "../productItem/ProductItem";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import ProductForm from "../productForm/ProductForm";
 import ChangeDollar from "../changeDollar/ChangeDollar";
 
 const Products = () => {
     const [products, setProducts] = useState(allProducts);
     const [dollar, setDollar] = useState(1);
+    const [showDollar, setShowDollar] = useState(true);
 
     const handleAddNewProduct = (newProduct) => {
         const randomCode = Math.random();
@@ -23,6 +24,10 @@ const Products = () => {
     return (
         <Container>
             <h1>List of Products</h1>
+            <Row md={4}>
+                <Button onClick={() => setShowDollar(!showDollar)}>Show dollar form</Button>
+                {showDollar && <ChangeDollar dollar={dollar} onChangeDollar={handleChangeDollar} />}
+            </Row>
             <Row>
                 {products.map((product) => (
                     <Col md={4} key={product.code} className='mb-2'>
@@ -37,7 +42,6 @@ const Products = () => {
             </Row>
             <Row md={4}>
                 <ProductForm onAddNewProduct={handleAddNewProduct} />
-                <ChangeDollar dollar={dollar} onChangeDollar={handleChangeDollar}/>
             </Row>
         </Container>
     );
